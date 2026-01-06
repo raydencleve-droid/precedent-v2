355101cbc2acdc1841ea33d2b8dd4c090d9ebe88
raydencleve-droid <raydencleve@gmail.com>
2026-01-07 01:38:47 +0700

    Update base URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/precedent-v2/",
+  base: "https://raydencleve-droid.github.io/precedent-v2/",
 });
+
